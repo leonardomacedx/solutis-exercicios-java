3155ce4074b4f556c9e3281046ccbef8d27a1d2a
Victor <victordosanjos19@gmail.com>
2023-08-07 19:02:18 -0300

    exercícios 7, 8 e 10

--- src/main/java/lista1/questao7/Main.java
+++ src/main/java/lista1/questao7/Main.java
@@ -1,4 +1,23 @@
 package main.java.lista1.questao7;
 
 public class Main {
+        public static void main(String[] args) {
+            System.out.println("Resultados usando o tipo int:");
+            for (int i = 1; i <= 100; i++) {
+                if (i % 3 == 0) {
+                    int resultado = i / 2;
+                    System.out.println("Múltiplo de 3: " + i + ", Divisão por 2: " + resultado);
+                }
+            }
+
+            System.out.println("\nResultados usando o tipo double:");
+            for (int i = 1; i <= 100; i++) {
+                if (i % 3 == 0) {
+                    double resultado = (double) i / 2;
+                    System.out.println("Múltiplo de 3: " + i + ", Divisão por 2: " + resultado);
+                }
+            }
+        }
+    }
+
 }
