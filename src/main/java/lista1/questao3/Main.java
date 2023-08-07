f13bab4f139c75975772f8204992cda7c291fdb3
Cainan Lima <cainan.lima25@gmail.com>
2023-08-07 17:24:07 -0300

    resolve questoes 3 e 4

--- src/main/java/lista1/questao3/Main.java
+++ src/main/java/lista1/questao3/Main.java
@@ -1,4 +1,27 @@
 package main.java.lista1.questao3;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a;
+        System.out.println("Digite um número inteiro:");
+        a = input.nextInt();
+        for(int i = 1; i <= a; i++){
+            if(isPrimo(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrimo(int x){
+        int divisores = 0;
+
+        for (int i = 1; i <= x; i++){
+            if(x % i == 0)
+                divisores++;
+        }
+        return (divisores == 2);
+    }
 }
--- src/main/java/lista1/questao4/Main.java
+++ src/main/java/lista1/questao4/Main.java
@@ -1,4 +1,14 @@
 package main.java.lista1.questao4;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float  mile;
+        System.out.println("Digite uma distância em milhas:");
+        mile = input.nextFloat();
+        double km = mile * 1.609;
+        System.out.printf("%.4f milhas = %.4fkm",mile , km);
+    }
 }
