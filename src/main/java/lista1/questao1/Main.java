e624f9202a876d1428811880e3c946726858ba27
Elio Rocha <elioneto21@gmail.com>
2023-08-07 18:23:36 -0300

    Questoes 01 e 02

--- src/main/java/lista1/questao1/Main.java
+++ src/main/java/lista1/questao1/Main.java
@@ -1,4 +1,16 @@
 package main.java.lista1.questao1;
 
 public class Main {
+
+    public static void main( String args[] ){
+        int i = 5;
+        int j = i;
+        j = 10;
+        System.out.println( "i + j = " + j + i );
+
+       // A saída será i + j = 105. O código acima está juntando a string com o j e com o i.
+    }
 }
+
+
+
--- src/main/java/lista1/questao2/Main.java
+++ src/main/java/lista1/questao2/Main.java
@@ -1,4 +1,25 @@
 package main.java.lista1.questao2;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextDouble();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextDouble();
+        if (a > b) {
+            System.out.println(a);
+        } else if (a == b) {
+
+            System.out.println("Os valores são iguais ");
+        } else {
+            System.out.println(b);
+
+        }
+    }
 }
