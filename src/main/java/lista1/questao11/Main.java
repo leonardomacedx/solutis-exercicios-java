917e1e5040dc3be8417b770c390540eb147a3cd1
Alisson <alisson.oliveira@ucsal.edu.br>
2023-08-07 19:41:20 -0300

    Add questão 11 e 12

--- src/main/java/lista1/questao11/Main.java
+++ src/main/java/lista1/questao11/Main.java
@@ -1,4 +1,32 @@
 package main.java.lista1.questao11;
 
+import java.util.Scanner;
+
+/*
+11. Escreva um programa que tenha duas palavras como entrada, e a saída
+imprima as palavras em ordem alfabética (utilize o método compareTo da
+classe String),. Informe também, qual das palavras tem o maior número de
+caracteres.
+ */
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String entradaUm = sc.nextLine(), entradaDois = sc.nextLine();
+
+        System.out.println("Em ordem alfabética: ");
+        if (entradaUm.compareTo(entradaDois) == 1){
+            System.out.print(entradaUm+", ");
+            System.out.println(entradaDois);
+        }else{
+            System.out.print(entradaDois+", ");
+            System.out.println(entradaUm);
+        }
+        if (entradaUm.length()> entradaDois.length()){
+            System.out.println("A maior string é: "+ entradaUm);
+        }else {
+            System.out.println("A maior string é: "+ entradaDois);
+        }
+    }
 }
--- src/main/java/lista1/questao12/Main.java
+++ src/main/java/lista1/questao12/Main.java
@@ -1,4 +1,83 @@
 package main.java.lista1.questao12;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+12. Os motoristas se preocupam com a quilometragem obtida por seus
+automóveis. Um motorista monitorou vários tanques cheios de gasolina registrando a
+quilometragem dirigida e a quantidade de combustível em litros utilizados para
+cada tanque cheio. Desenvolva um aplicativo Java que receba como entrada
+os quilômetros dirigidos e os litros de gasolina consumidos (ambos com
+inteiros) para cada tanque cheio. O programa deve calcular e exibir o consumo
+em quilômetros/litro para cada tanque cheio e imprimir a quilometragem
+combinada e a soma total de litros de combustível consumidos até esse ponto.
+Todos os cálculos de média devem produzir resultados de ponto flutuante.
+ */
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem vindo ao seu app de contabilização!");
+        String escolhaMenu;
+        int quilometro, gasolina;
+        List<Tanque> tanques = new ArrayList<>();
+
+        System.out.println("---------------------------------");
+        System.out.println("Digite:");
+        System.out.println("---------");
+        System.out.println("1 - Adicionar um novo tanque");
+        System.out.println("4 - Encerrar programa");
+        System.out.println("---------------------------------");
+
+        escolhaMenu = sc.nextLine();
+
+        while(!escolhaMenu.equals("4")){
+            System.out.println("Digite quantos quilômetros foram dirigidos:");
+            quilometro = sc.nextInt();
+            System.out.println("Digite o valor de gasolina consumido:");
+            gasolina = sc.nextInt();
+            tanques.add(new Tanque(quilometro, gasolina));
+
+            System.out.println("---------------------------------");
+            System.out.println("Digite:");
+            System.out.println("---------");
+            System.out.println("1 - Adicionar um novo tanque");
+            System.out.println("4 - Encerrar programa");
+            System.out.println("---------------------------------");
+
+            escolhaMenu = sc.next();
+        }
+
+        for (int i = 0; i < tanques.size() ; i++) {
+            System.out.println("Para o tanque "+ (i+1)+", o consumo de km por litro foi: "
+                    + (float) tanques.get(i).getQuilometros()/(float) tanques.get(i).getGasolina() +" km/l");
+        }
+    }
+}
+
+class Tanque{
+    private int quilometros;
+    private int gasolina;
+
+    public Tanque(int quilometros, int gasolina) {
+        this.quilometros = quilometros;
+        this.gasolina = gasolina;
+    }
+
+    public int getQuilometros() {
+        return quilometros;
+    }
+
+    public void setQuilometros(int quilometros) {
+        this.quilometros = quilometros;
+    }
+
+    public int getGasolina() {
+        return gasolina;
+    }
+
+    public void setGasolina(int gasolina) {
+        this.gasolina = gasolina;
+    }
 }
